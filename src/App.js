1ac797892754e8f24d25cf0bccfdf263edeabd44
Miłosz <57639228+miloszAlejster@users.noreply.github.com>
2022-05-29 18:29:53 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Dot from "./Dot";
+import React, { useState } from 'react';
 
 function App() {
+    const[idDotted, setDot] = useState();
+    const hsetDot = (key) => {
+        setDot(key);
+    }
     return (
         <div className="dots-wrapper">
             {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
+                <Dot 
+                    key={i}
+                    id={i}
+                    idDotted={idDotted}
+                    hsetDot={hsetDot}
+                />
             ))}
         </div>
     );
--- src/Dot.js
+++ src/Dot.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Dot.css';
 
-const Dot = () => {
-    // TODO: put your code here
+const Dot = ({id, idDotted, hsetDot}) => {
+    const viewColor = () => {
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        document.documentElement.style.setProperty('--bg-color', randomColor);
+    };
+    const viewDot = () => {
+        hsetDot(id);
+    }
+    const hClick = () =>{
+        viewColor();
+        viewDot();
+    }
+    const view_number = (id === idDotted) ? id+1 : null;
+    const class_flag = (id === idDotted);
     return (
-        <div className="dot"/>
+        <div className={class_flag ? "dotted" : "dot"} onClick={hClick}>
+            <p>{view_number}</p>
+        </div>
     );
 };
 
